0314c7df1210f8d6e32ee9f177c042040555bb42
Alla <1304alla@gmail.com>
2023-03-07 09:03:35 +0700

    added function object

---
+++ 6__аdvanced_work_with_functions/6.6/task-2/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+function sum(a) {
+    let currentSum = a;
+    function f(b) {
+        currentSum += b;
+        return f;
+    };
+
+    f.toString = function () {
+        return currentSum;
+    };
+
+    return f;
+
+
+
+}
+
+alert(sum(0)(1)(2)(3)(4)(5)(5));
+
+
+
