c222b0c76ea180f95d88dd65962d0d116d569350
Alla <1304alla@gmail.com>
2023-03-08 18:23:37 +0700

    DOM navigation

---
+++ browser/1__document/1.3/task-1/script.js
@@ -0,0 +1,5 @@
+"use strict";
+
+let div = document.body.firstElementChild;
+let ul = div.nextElementSibling;
+let pit = ul.lastElementChild;
---
+++ browser/1__document/1.3/task-2/script.js
@@ -0,0 +1,4 @@
+"use strict";
+
+//Правда, что elem.lastChild.nextSibling всегда равен null? //Правда
+//Правда, что elem.children[0].previousSibling всегда равен null ?//Нет, перед children[0] может быть узел,который не является элементом
