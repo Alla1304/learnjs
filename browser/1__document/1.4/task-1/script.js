a2c6bad8810c320a5e1ee6043700d89e2d7d30b6
Alla <1304alla@gmail.com>
2023-03-08 18:25:36 +0700

    added search

---
+++ browser/1__document/1.4/task-1/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+let table = document.getElementById('age-table');
+let label = table.querySelectorAll('label');
+let age = table.querySelector('td');
+let search = document.getElementsByName('search')[0];
+let firstInput = search.querySelector('input');
+let inputs = search.querySelectorAll('input');
+inputs[inputs.length - 1];
